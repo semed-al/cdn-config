53e49ef76a7ee43ccd2ba664912db9ad21e62282
Williams <williamsalcantara@gmail.com>
2022-08-05 10:14:09 -0300

    gerando boletim escolar de turmas multisseriadas

--- i-educar-reports-package/ieducar/Queries/GeneralOpinionsTrait.php
+++ i-educar-reports-package/ieducar/Queries/GeneralOpinionsTrait.php
@@ -10,13 +10,12 @@ trait GeneralOpinionsTrait
         $matricula = $this->args['matricula'];
         $turma = $this->args['turma'];
         $serie = $this->args['serie'];
-        $turma = $this->args['turma'];
         $instituicao = $this->args['instituicao'];
         $escola = $this->args['escola'];
         $ano = $this->args['ano'];
         $curso = $this->args['curso'];
         $situacao_matricula = $this->args['situacao_matricula'];
-        $etapa = $this->args['etapa'];
+        // $etapa = $this->args['etapa'];
         $alunos_diferenciados = $this->args['alunos_diferenciados'];
 
         return <<<SQL
@@ -103,8 +102,6 @@ trait GeneralOpinionsTrait
             AND serie.ativo = 1
         INNER JOIN pmieducar.turma AS turma
             ON turma.ref_ref_cod_escola = escola.cod_escola
-            AND turma.ref_cod_curso = escola_curso.ref_cod_curso
-            AND turma.ref_ref_cod_serie = escola_serie.ref_cod_serie
             AND turma.ativo = 1
         JOIN modules.regra_avaliacao_serie_ano rasa
           ON serie.cod_serie = rasa.serie_id
@@ -120,7 +117,9 @@ trait GeneralOpinionsTrait
         INNER JOIN
             pmieducar.matricula AS m
             ON m.cod_matricula = mt.ref_cod_matricula
-                          AND m.ano = turma.ano
+                        AND m.ref_cod_curso = curso.cod_curso
+                        AND m.ref_ref_cod_serie = serie.cod_serie
+                        AND m.ano = turma.ano
         INNER JOIN
             pmieducar.aluno AS aluno
             ON aluno.cod_aluno = m.ref_cod_aluno
