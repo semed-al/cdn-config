aa6d3c32de1569b31eee5a46ff20bd0542778849
Williams <williamsalcantara@gmail.com>
2022-11-30 23:53:08 -0300

    filtrando apenas séries da escola no histórico

--- i-educar-reports-package/ieducar/Queries/SchoolHistorySeriesYearsTrait.php
+++ i-educar-reports-package/ieducar/Queries/SchoolHistorySeriesYearsTrait.php
@@ -302,62 +302,62 @@ trait SchoolHistorySeriesYearsTrait
                     AND phe1.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd1 ON hd1.ref_ref_cod_aluno = vhsa.cod_aluno
                     AND trim(relatorio.get_texto_sem_caracter_especial(hd1.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
-                    AND hd1.ref_sequencial = phe1.sequencial AND hd1.ref_sequencial = 1 
+                    AND hd1.ref_sequencial = phe1.sequencial
                 
                 LEFT JOIN pmieducar.historico_escolar phe2 ON phe2.ref_cod_aluno = vhsa.cod_aluno AND phe2.ativo = 1 
                     AND ((phe2.posicao is not null AND phe2.posicao = 2) OR (phe2.ano = vhsa.ano_2serie AND phe2.nm_serie LIKE '%2%'))
                     AND phe2.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd2 ON hd2.ref_ref_cod_aluno = vhsa.cod_aluno
                     AND trim(relatorio.get_texto_sem_caracter_especial(hd2.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
-                    AND hd2.ref_sequencial = phe2.sequencial AND hd2.ref_sequencial = 2
+                    AND hd2.ref_sequencial = phe2.sequencial
                 
                 LEFT JOIN pmieducar.historico_escolar phe3 ON phe3.ref_cod_aluno = vhsa.cod_aluno AND phe3.ativo = 1 
                     AND ((phe3.posicao is not null AND phe3.posicao = 3) OR (phe3.ano = vhsa.ano_3serie AND phe3.nm_serie LIKE '%3%'))
                     AND phe3.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd3 ON hd3.ref_ref_cod_aluno = vhsa.cod_aluno
                     AND trim(relatorio.get_texto_sem_caracter_especial(hd3.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
-                    AND hd3.ref_sequencial = phe3.sequencial AND hd3.ref_sequencial = 3
+                    AND hd3.ref_sequencial = phe3.sequencial
                 
                 LEFT JOIN pmieducar.historico_escolar phe4 ON phe4.ref_cod_aluno = vhsa.cod_aluno AND phe4.ativo = 1 
                     AND ((phe4.posicao is not null AND phe4.posicao = 4) OR (phe4.ano = vhsa.ano_4serie AND phe4.nm_serie LIKE '%4%'))
                     AND phe4.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd4 ON hd4.ref_ref_cod_aluno = vhsa.cod_aluno
                     AND trim(relatorio.get_texto_sem_caracter_especial(hd4.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
-                    AND hd4.ref_sequencial = phe4.sequencial AND hd4.ref_sequencial = 4
+                    AND hd4.ref_sequencial = phe4.sequencial
                 
                 LEFT JOIN pmieducar.historico_escolar phe5 ON phe5.ref_cod_aluno = vhsa.cod_aluno AND phe5.ativo = 1 
                     AND ((phe5.posicao is not null AND phe5.posicao = 5) OR (phe5.ano = vhsa.ano_5serie AND phe5.nm_serie LIKE '%5%'))
                     AND phe5.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd5 ON hd5.ref_ref_cod_aluno = vhsa.cod_aluno
                     AND trim(relatorio.get_texto_sem_caracter_especial(hd5.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
-                    AND hd5.ref_sequencial = phe5.sequencial AND hd5.ref_sequencial = 5
+                    AND hd5.ref_sequencial = phe5.sequencial
                 
                 LEFT JOIN pmieducar.historico_escolar phe6 ON phe6.ref_cod_aluno = vhsa.cod_aluno AND phe6.ativo = 1 
                     AND ((phe6.posicao is not null AND phe6.posicao = 6) OR (phe6.ano = vhsa.ano_6serie AND phe6.nm_serie LIKE '%6%'))
                     AND phe6.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd6 ON hd6.ref_ref_cod_aluno = vhsa.cod_aluno
-                    AND hd6.ref_sequencial = phe6.sequencial AND hd6.ref_sequencial = 6 
+                    AND hd6.ref_sequencial = phe6.sequencial
                     AND trim(relatorio.get_texto_sem_caracter_especial(hd6.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
 
                 LEFT JOIN pmieducar.historico_escolar phe7 ON phe7.ref_cod_aluno = vhsa.cod_aluno AND phe7.ativo = 1 
                     AND ((phe7.posicao is not null AND phe7.posicao = 7) OR (phe7.ano = vhsa.ano_7serie AND phe7.nm_serie LIKE '%7%'))
                     AND phe7.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd7 ON hd7.ref_ref_cod_aluno = vhsa.cod_aluno
-                    AND hd7.ref_sequencial = phe7.sequencial AND hd7.ref_sequencial = 7 
+                    AND hd7.ref_sequencial = phe7.sequencial
                     AND TRIM(relatorio.get_texto_sem_caracter_especial(hd7.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
 
                 LEFT JOIN pmieducar.historico_escolar phe8 ON phe8.ref_cod_aluno = vhsa.cod_aluno AND phe8.ativo = 1 
                     AND ((phe8.posicao is not null AND phe8.posicao = 8) OR (phe8.ano = vhsa.ano_8serie AND phe8.nm_serie LIKE '%8%'))
                     AND phe8.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd8 ON hd8.ref_ref_cod_aluno = vhsa.cod_aluno
-                    AND hd8.ref_sequencial = phe8.sequencial AND hd8.ref_sequencial = 8 
+                    AND hd8.ref_sequencial = phe8.sequencial
                     AND TRIM(relatorio.get_texto_sem_caracter_especial(hd8.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
                     
                 LEFT JOIN pmieducar.historico_escolar phe9 ON phe9.ref_cod_aluno = vhsa.cod_aluno AND phe9.ativo = 1 
                     AND ((phe9.posicao is not null AND phe9.posicao = 9) OR (phe9.ano = vhsa.ano_9serie AND phe9.nm_serie LIKE '%9%'))
                     AND phe9.ano <= max_ano.ano
                 LEFT JOIN pmieducar.historico_disciplinas hd9 ON hd9.ref_ref_cod_aluno = vhsa.cod_aluno
-                    AND hd9.ref_sequencial = phe9.sequencial AND hd9.ref_sequencial = 9 
+                    AND hd9.ref_sequencial = phe9.sequencial
                     AND TRIM(relatorio.get_texto_sem_caracter_especial(hd9.nm_disciplina)) = trim(relatorio.get_texto_sem_caracter_especial(vhsa.disciplina)) 
                 LEFT JOIN modules.educacenso_cod_aluno eca ON (eca.cod_aluno = aluno.cod_aluno)
                 LEFT JOIN public.municipio ON (municipio.idmun = fisica.idmun_nascimento)
